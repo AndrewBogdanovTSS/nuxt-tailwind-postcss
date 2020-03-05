4396b0069ede9fb307d223c4a0f0add21f42fded
Andrey Bogdanov <andrey@wonder.shop>
2020-03-05 12:11:19 +0300

    adding config examples

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,21 @@
 ** Docs: https://tailwindcss.com/docs/configuration
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
+
+// byKeys will return keys instead of values to the passed function
+function mapObject(obj, fn, byKeys = false) {
+  return Object.keys(obj).reduce((result, key) => {
+    result[key] = byKeys ? fn(key) : fn(obj[key])
+    return result
+  }, {})
+}
+
 module.exports = {
-  theme: {},
+  theme: {
+    colors: require('./config/colors'),
+    // Example of generating screen sizes based on the external json config
+    screens:mapObject(require('./config/breakpoints'), size => `${size}px`)
+  },
   variants: {},
   plugins: []
 }
